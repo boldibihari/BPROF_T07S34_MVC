177aab900d7ce32c4a2c727b0cb9b61675843741
Boldi Bihari <boldibihari@gmail.com>
2021-05-22 22:09:37 +0200

    IAuthLogic done

---
+++ NationalChampionship/NationalChampionship.Logic/Interfaces/IAuthLogic.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NationalChampionship.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NationalChampionship.Logic.Interfaces
+{
+    public interface IAuthLogic
+    {
+        IEnumerable<IdentityUser> GetAllUser();
+
+        Task<string> RegisterUser(RegisterViewModel model);
+
+        Task<TokenViewModel> LoginUser(LoginViewModel model);
+    }
+}
