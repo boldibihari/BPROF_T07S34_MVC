b525928a932eabb9c22ffe6905c04f8fd938ec82
Boldi Bihari <boldibihari@gmail.com>
2021-05-22 22:10:27 +0200

    AuthLogic done

---
+++ NationalChampionship/NationalChampionship.Logic/Classes/AuthLogic.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using NationalChampionship.Data.Models;
+using NationalChampionship.Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NationalChampionship.Logic.Classes
+{
+    public class AuthLogic : IAuthLogic
+    {
+        UserManager<IdentityUser> userManager;
+        RoleManager<IdentityRole> roleManager;
+
+        public AuthLogic(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public IEnumerable<IdentityUser> GetAllUser()
+        {
+            return this.userManager.Users;
+        }
+
+        public async Task<string> RegisterUser(RegisterViewModel model)
+        {
+            var user = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, "User");
+            }
+
+            return user.UserName;
+        }
+
+        public async Task<TokenViewModel> LoginUser(LoginViewModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Username);
+            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claims = new List<Claim>
+                {
+                  new Claim(JwtRegisteredClaimNames.Sub, model.Username),
+                  new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                  new Claim(ClaimTypes.NameIdentifier, user.Id)
+                };
+
+                var roles = await userManager.GetRolesAsync(user);
+
+                claims.AddRange(roles.Select(role => new Claim(ClaimsIdentity.DefaultRoleClaimType, role)));
+
+                var signinKey = new SymmetricSecurityKey(
+                  Encoding.UTF8.GetBytes("Paris Berlin Cairo Sydney Tokyo Beijing Rome London Athens"));
+
+                var token = new JwtSecurityToken(
+                  issuer: "http://www.security.org",
+                  audience: "http://www.security.org",
+                  claims: claims,
+                  expires: DateTime.Now.AddMinutes(60),
+                  signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return new TokenViewModel
+                {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    Expiration = token.ValidTo
+                };
+            }
+
+            throw new ArgumentException("Login failed");
+        }
+    }
+}
