9848d98f9643952d971304e20d88cd369be0d6ec
Boldi Bihari <boldibihari@gmail.com>
2021-05-24 13:47:21 +0200

    Wpf created

---
+++ NationalChampionship/NationalChampionship.WpfClient/LoginWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using NationalChampionship.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NationalChampionship.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public string Token { get; private set; }
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RestService restservice = new RestService("https://localhost:5001/", "/Auth");
+                TokenViewModel tvm = await restservice.Put<TokenViewModel, LoginViewModel>(new LoginViewModel()
+                {
+                    Username = username.Text,
+                    Password = password.Password
+                });
+                Token = tvm.Token;
+                this.DialogResult = true;
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Wrong password or username!");
+            }
+        }
+
+        private void RegisterButtonClick(object sender, RoutedEventArgs e)
+        {
+            RestService restservice = new RestService("https://localhost:5001/", "/Auth");
+            restservice.Post<RegisterViewModel>(new RegisterViewModel()
+            {
+                Email = username.Text,
+                Password = password.Password
+            });
+
+            MessageBox.Show("You can log in now!");
+        }
+    }
+}
---
+++ NationalChampionship/NationalChampionship.WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using NationalChampionship.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NationalChampionship.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string token;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Login();
+        }
+
+        public async Task Login()
+        {
+            LoginWindow login = new LoginWindow();
+            if (login.ShowDialog() == true)
+            {
+                token = login.Token;
+                await this.GetAllClub();
+            }
+        }
+
+        public async Task GetAllClub()
+        {
+            cbox.ItemsSource = null;
+            RestService restservice = new RestService("https://localhost:5001/", "/User/GetAllCLub", token);
+            IEnumerable<Club> clubs = await restservice.Get<Club>();
+
+            cbox.ItemsSource = clubs;
+            cbox.SelectedIndex = 0;
+        }
+    }
+}
