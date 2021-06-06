1d7b45e609ffbdbe34e399f744f5ca26ba7ace12
Boldi Bihari <boldibihari@gmail.com>
2021-06-06 10:55:41 +0200

    UpdateClubWindow done

--- NationalChampionship/NationalChampionship.WpfClient/ClubWindows/UpdateClubWindow.xaml.cs
+++ NationalChampionship/NationalChampionship.WpfClient/ClubWindows/UpdateClubWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace NationalChampionship.WpfClient
+namespace NationalChampionship.WpfClient.ClubWindows
 {
     /// <summary>
     /// Interaction logic for UpdateClubWindow.xaml
