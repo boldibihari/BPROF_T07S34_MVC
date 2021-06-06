da75ce07843c33ac6501aa7d1ef3a7936463ecea
Boldi Bihari <boldibihari@gmail.com>
2021-06-06 10:58:33 +0200

    AddManagerWindow done

--- NationalChampionship/NationalChampionship.WpfClient/ManagerWindows/AddManagerWindow.xaml.cs
+++ NationalChampionship/NationalChampionship.WpfClient/ManagerWindows/AddManagerWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace NationalChampionship.WpfClient
+namespace NationalChampionship.WpfClient.ManagerWindows
 {
     /// <summary>
     /// Interaction logic for AddManagerWindow.xaml
